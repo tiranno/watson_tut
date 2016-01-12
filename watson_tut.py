593bedac9ffbecade6056375b0faf593bd026df1
Brian Kelly <briankelly@mac-briankelly.corp.sgi.com>
2016-01-12 18:45:52 -0500

    babysteps

--- watson_tut.py
+++ watson_tut.py
@@ -5,3 +5,10 @@ import urllib2
 
 if len(sys.argv) != 2:
   print('Usage: ./watson_prog <hello in your language of choice>')
+
+# curl -u "9fdda5be-7c24-4565-a13b-341d5098d806":"IaevO0SytB0m"  "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
+URL = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
+USER = "9fdda5be-7c24-4565-a13b-341d5098d806"
+PASS = "IaevO0SytB0m"
+
+print ()
