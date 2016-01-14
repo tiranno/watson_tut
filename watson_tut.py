253743a8dbe8374f7f4134d2e6bee3336b8ed7fe
Brian Kelly <briankelly@mac-briankelly.corp.sgi.com>
2016-01-14 17:43:36 -0500

    basic request working

--- watson_tut.py
+++ watson_tut.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
 import sys
+import urllib
 import urllib2
 
 if len(sys.argv) != 2:
   print('Usage: ./watson_prog <hello in your language of choice>')
 
-# curl -u "9fdda5be-7c24-4565-a13b-341d5098d806":"IaevO0SytB0m"  "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
-URL = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
 USER = "9fdda5be-7c24-4565-a13b-341d5098d806"
 PASS = "IaevO0SytB0m"
 
-print ()
+# curl -u "9fdda5be-7c24-4565-a13b-341d5098d806":"IaevO0SytB0m"  "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
+url = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
+auth = {USER : PASS}
+
+passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+passman.add_password(None, url, USER, PASS)
+authhandler = urllib2.HTTPBasicAuthHandler(passman)
+opener = urllib2.build_opener(authhandler)
+urllib2.install_opener(opener)
+
+response = urllib2.urlopen(url).read()
+print(response)
