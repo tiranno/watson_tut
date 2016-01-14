8040f546adca01a1f620a8b81cc05e1e83ec8663
tiranno <pedelose.1@osu.edu>
2016-01-14 17:56:18 -0500

    Moved POST req to function

--- watson_tut.py
+++ watson_tut.py
@@ -4,21 +4,28 @@ import sys
 import urllib
 import urllib2
 
+
+def f_call_watson (url, query):
+    USER = "9fdda5be-7c24-4565-a13b-341d5098d806"
+    PASS = "IaevO0SytB0m"
+
+    passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passman.add_password(None, url, USER, PASS)
+    authhandler = urllib2.HTTPBasicAuthHandler(passman)
+    opener = urllib2.build_opener(authhandler)
+    urllib2.install_opener(opener)
+
+
 if len(sys.argv) != 2:
   print('Usage: ./watson_prog <hello in your language of choice>')
 
-USER = "9fdda5be-7c24-4565-a13b-341d5098d806"
-PASS = "IaevO0SytB0m"
-
 # curl -u "9fdda5be-7c24-4565-a13b-341d5098d806":"IaevO0SytB0m"  "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
-url = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
-auth = {USER : PASS}
+# url = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
+# f_call_watson(url, "")
 
-passman = urllib2.HTTPPasswordMgrWithDefaultRealm()
-passman.add_password(None, url, USER, PASS)
-authhandler = urllib2.HTTPBasicAuthHandler(passman)
-opener = urllib2.build_opener(authhandler)
-urllib2.install_opener(opener)
+url = "https://gateway.watsonplatform.net/language-translation/api/v2/identify?text=come"
+f_call_watson(url, "")
+
 
 response = urllib2.urlopen(url).read()
 print(response)
