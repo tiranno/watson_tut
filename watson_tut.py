5ccfa9512fc892b8ca6c6878b617d82416e9b11d
Brian Kelly <briankelly@mac-briankelly.corp.sgi.com>
2016-01-14 18:21:21 -0500

    wikipedia opens with full dict

--- watson_tut.py
+++ watson_tut.py
@@ -4,7 +4,8 @@ import sys
 import urllib
 import urllib2
 import json
-
+import wikipedia
+import webbrowser
 
 def f_call_watson (url, query):
     USER = "9fdda5be-7c24-4565-a13b-341d5098d806"
@@ -18,7 +19,7 @@ def f_call_watson (url, query):
     return urllib2.urlopen(url).read()
 
 if len(sys.argv) != 2:
-  print('Usage: ./watson_tut <hello in your language of choice>')
+  print("Usage: ./watson_tut <hello in your language of choice>")
   sys.exit()
 
 url = "https://gateway.watsonplatform.net/language-translation/api/v2/identifiable_languages"
@@ -27,5 +28,11 @@ ident_lang = json.loads(f_call_watson(url, ""))
 url = "https://gateway.watsonplatform.net/language-translation/api/v2/identify?text=" + sys.argv[1]
 lang_code = f_call_watson(url, "")
 
-print(ident_lang)
+language = ''
+for ent in ident_lang[u'languages']:
+    if ent[u'language'] == lang_code:
+        language = ent[u'name']
+
 print(lang_code)
+print(language)
+webbrowser.open("https://en.wikipedia.org/wiki/" + language + "_language")
